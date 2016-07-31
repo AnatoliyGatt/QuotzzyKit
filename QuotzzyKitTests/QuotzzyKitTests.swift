6b2f96a0cdfb30aa3895652b04b3db7c8b4b75e3
Anatoliy Gatt <anatoliy.gatt@aol.com>
2016-07-31 20:42:59 +0200

    Fix 'if statement' coding style in QuotzzyKitTests.swift

--- QuotzzyKitTests/QuotzzyKitTests.swift
+++ QuotzzyKitTests/QuotzzyKitTests.swift
@@ -38,7 +38,7 @@ class QuotzzyKitTests: XCTestCase {
         let expectation = expectationWithDescription("Quote request should succeed")
         var quoteObject: Quote?
         Quotzzy.sharedClient().getQuote(nil, key: nil, completionHandler: {(quote: Quote?, error: NSError?) -> Void in
-            if (error == nil) {
+            if error == nil {
                 quoteObject = quote
                 expectation.fulfill()
             } else {
